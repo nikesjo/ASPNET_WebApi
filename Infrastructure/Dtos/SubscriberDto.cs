fbe51ff4bfa32995b41824080ea07abcd705985a
Nike <nikesjolin@outlook.com>
2024-03-21 16:55:07 +0100

    created subscriberdto

---
+++ Infrastructure/Dtos/SubscriberDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class SubscriberDto
+{
+    [Required]
+    public string Email { get; set; } = null!;
+
+    public bool DailyNewsLetter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+}
