53dbdd35ad0a5b03883a8657b87366d2bae4ab73
Nike <nikesjolin@outlook.com>
2024-03-21 17:07:47 +0100

    Created SubscriberEntity

---
+++ Infrastructure/Entities/SubscriberEntity.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class SubscriberEntity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
+    public string Email { get; set; } = null!;
+
+    public bool DailyNewsLetter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+
+    public static implicit operator SubscriberEntity(SubscriberDto dto)
+    {
+        return new SubscriberEntity
+        {
+            Email = dto.Email,
+            DailyNewsLetter = dto.DailyNewsLetter,
+            AdvertisingUpdates = dto.AdvertisingUpdates,
+            WeekInReview = dto.WeekInReview,
+            EventUpdates = dto.EventUpdates,
+            StartupsWeekly = dto.StartupsWeekly,
+            Podcasts = dto.Podcasts
+        };
+    }
+}
