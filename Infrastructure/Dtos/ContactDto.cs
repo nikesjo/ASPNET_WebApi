99690614078759417241cd2ff4a8e45b2bdc7e14
Nike <nikesjolin@outlook.com>
2024-03-22 15:44:59 +0100

    Created ContactEntity and ContactDto

---
+++ Infrastructure/Dtos/ContactDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class ContactDto
+{
+    [Required]
+    public string FullName { get; set; } = null!;
+
+    [Required]
+    public string Email { get; set; } = null!;
+
+    public string? ServiceOption { get; set; }
+
+    [Required]
+    public string Message { get; set; } = null!;
+}
---
+++ Infrastructure/Entities/ContactEntity.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class ContactEntity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
+    public string FullName { get; set; } = null!;
+
+    [Required]
+    public string Email { get; set; } = null!;
+
+    public string? ServiceOption { get; set; }
+
+    [Required]
+    public string Message { get; set; } = null!;
+
+    public static implicit operator ContactEntity(ContactDto dto)
+    {
+        return new ContactEntity
+        {
+            FullName = dto.FullName,
+            Email = dto.Email,
+            ServiceOption = dto.ServiceOption,
+            Message = dto.Message
+        };
+    }
+}
